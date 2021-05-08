8e65b411b164efcc16a08e9a02add4cd10361a34
Joseph Bacal <bacalj@gmail.com>
2021-05-08 17:06:23 -0400

    oh updated config now for nuxt 2.9

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import tailwindTypography from '@tailwindcss/typography'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -31,9 +33,8 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/sanity'
+    '@nuxtjs/sanity/module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
