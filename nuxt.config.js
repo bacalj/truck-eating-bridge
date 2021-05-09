745e0b7dd8383258c2600aebf5fb38b9dd7f040c
Joseph Bacal <bacalj@gmail.com>
2021-05-09 10:11:21 -0400

    site title meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'truck-eating-bridge',
+    title: 'Truck-Eating-Bridge Solutions',
     htmlAttrs: {
       lang: 'en'
     },
