03f627bc139be779c60db69cb5a68bc1c7dda255
Joseph Bacal <bacalj@gmail.com>
2021-05-18 09:59:49 -0400

    jules about text

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="flex justify-center">
     <div class="w-2/3">
-      <h2 class="py-6 font-medium text-2xl">About this Project</h2>
-      <p>About content here</p>
-    </div>
 
+      <h2 class="2xl">About This Project:</h2>
+      <p class>The 5E 5TH Grade Class At CSSC Is Working On An Engineering & Design Project About The Truck Eating Brige In Northampton, MA, 01060, U.S.A. The Problem Of The T.E.B. Is That Truckers Think They Can Fit Under The Bridge But Cant And End Up Crashing.</p>
+
+
+      <h2 class="2xl mt-4">About The Class:</h2>
+      <p>The Class Of 5E CSSC Is Made Up Of 18 Students All Working Hard To Establish A Bridge That Will Let Trucks Go Under The T.E.B.</p>
+    </div>
   </div>
 
 </template>
@@ -18,6 +22,10 @@ export default {
 
 <style lang="postcss" scoped>
 h2 {
+  font-family: 'Cabin Sketch', sans-serif;
+  font-weight: 600;
+}
+p {
   font-family: 'Cabin Sketch', sans-serif;
 }
 </style>
