156c41b860b1abbb923e89e1c8edbb848dfbb049
Joseph Bacal <bacalj@gmail.com>
2021-05-20 17:42:13 -0400

    adjust size iframe

---
+++ pages/games.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex justify-center">
+    <div class="w-2/3">
+
+      <h2 class="2xl">Games</h2>
+
+    </div>
+  </div>
+
+</template>
+
+
+<style lang="postcss" scoped>
+h2 {
+  font-family: 'Cabin Sketch', sans-serif;
+  font-weight: 600;
+}
+p {
+  font-family: 'Cabin Sketch', sans-serif;
+}
+</style>
--- pages/problem.vue
+++ pages/problem.vue
@@ -4,7 +4,7 @@
       <h2 class="py-6 font-medium text-2xl">The Truck-Eating Bridge Problem</h2>
       <p>The problem with these bridges is that they're too short. The obvious reasons, raising the bridge or moving the road down, still don't work because of the railroad on the red bridge, and the sewer below the road.
       </p>
-      <iframe src="https://www.tinkercad.com/embed/5mXcKpyPzDK?editbtn=1" frameborder="0" height="500" width="500"></iframe>
+      <iframe class="my-6" src="https://www.tinkercad.com/embed/5mXcKpyPzDK?editbtn=1" frameborder="0" height="500" width="100%"></iframe>
       <h2>what is the T.E.B.(truck eating bridge)?</h2>
       <p>The Truck-Eating Bridge(s) are these bridges that can't be moved up or down. They surround Northampton MA, and provide a dillema for any trailers coming in.
         What usually happens is that the trucks come into Northampton to go to Industrial Drive to get to the Coke factory. They’re usually not from the area around or in Northampton, so when the GPS tells them to go through Main Street and though the bridge, they don't know that they're going to crash. So they crash.
