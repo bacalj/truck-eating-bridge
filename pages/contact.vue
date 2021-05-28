c61399669187a6db65147c514d9ab35ccd8c39fd
Joseph Bacal <bacalj@gmail.com>
2021-05-27 21:08:16 -0400

    add paul to contacts

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,6 +3,7 @@
     <div class="w-2/3">
       <h2 class="py-6 font-medium text-2xl">Contact Us</h2>
       <p>Joe Bacal: <b>jebacal@smith.edu</b></p>
+      <p>Paul Matylas: <b>pmatylas@smith.edu</b></p>
       <div>
         <img src="https://res.cloudinary.com/jbmtl75/image/upload/v1622078602/arc/truck_gif_gwvilx.gif" alt="">
       </div>
